d56fda4716418a349ab2e34d5c02f03805e531c9
Quan <hoilamgi2712003@gmail.com>
2023-10-20 00:25:43 +0700

    menu

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame() {
+        SceneManager.LoadScene("Pacman");
+    }
+    public void QuitGame() {
+        Application.Quit();
+        Debug.Log("quit game");        
+    }
+}
